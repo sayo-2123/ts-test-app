d199de5b53d29bcbc453a309a6f2ac87ecee4e1b
sayo-2123 <classa1990@gmail.com>
2024-05-01 16:03:51 +0900

    課題4実装

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from './header.tsx';
 import { Lists } from './components/lists.tsx';
 import { Article } from './pages/articlePage.tsx';
 import { Routes, Route } from 'react-router-dom';
+import { Contact } from './pages/contactPage.tsx';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Lists />} />
         <Route path='/article/:id' element={<Article />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </>
   );
